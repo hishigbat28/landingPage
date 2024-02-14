4636a06c98b65f39dfc3a6d8db8718a9abe5201f
hishigbat28 <hishigbat28@gmail.com>
2024-02-14 14:22:34 +0800

    mobile menu nemsen

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
-(function () {
-  'use strict';
+;(function () {
+  'use strict'
 
   // eslint-disable-next-line no-undef
-  AOS.init();
+  AOS.init()
 
-  console.log('hello world');
-  var link = document.querySelectorAll('.nav-link');
+  console.log('hello world')
+  var link = document.querySelectorAll('.nav-link')
   if (link) {
     link.forEach(function (item) {
       item.addEventListener('click', function (e) {
-        console.log(e.currentTarget);
-      });
-    });
+        console.log(e.currentTarget)
+      })
+    })
   }
-})();
+})()
+
+// Menu
+var menuToggle = document.querySelector('.menu-button')
+if (menuToggle) {
+  menuToggle.addEventListener('click', function () {
+    if (!document.body.classList.contains('show-mobile-menu')) {
+      document.body.classList.add('show-mobile-menu')
+    } else {
+      document.body.classList.remove('show-mobile-menu')
+    }
+  })
+}
